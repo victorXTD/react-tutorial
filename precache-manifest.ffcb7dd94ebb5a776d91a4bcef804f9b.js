974394615353141d473c3fe684af2298462b915d
VictorXiao <843731025@qq.com>
2020-06-11 13:13:36 +0900

    Updates

---
+++ precache-manifest.ffcb7dd94ebb5a776d91a4bcef804f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b92d2f23093a9d77e21a112d1e797e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "d4894a7d30200a2b402f",
+    "url": "/react-tutorial/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "d4894a7d30200a2b402f",
+    "url": "/react-tutorial/static/js/2.e936030a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.e936030a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d7e4a26706b87798a3",
+    "url": "/react-tutorial/static/js/main.24bb96fc.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
